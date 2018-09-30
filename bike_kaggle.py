82cbec6a872d009e7e2a83f81697531865163561
金国卿 GuoqingJin <guoqingjin@synacast.local>
2018-09-30 16:51:13 +0800

    算法预测

--- bike_kaggle.py
+++ bike_kaggle.py
@@ -8,7 +8,6 @@ from datetime import datetime
 import test_fun
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-from sklearn.feature_extraction import DictVectorizer
 import matplotlib.pyplot as plt
  
 # print(test_fun.sum(1,2))
@@ -39,6 +38,7 @@ def time_clean(df):
     df['year'] = temp.year
     df['date'] = temp.date
     df['time'] = temp.time
+    df['month'] = temp.month
 
     # 获取一周中的第几天
     weekday = [datetime.date(datetime.strptime(time,'%Y-%M-%d')).isoweekday() for time in df['datetime'].str[:10]]
@@ -82,11 +82,10 @@ df = time_clean(df)
 
 conti_list = ['temp','atemp','humidity','windspeed']
 conti_standard(df,conti_list)
-cols = ['count','year','weekday','hour','season','holiday','workingday','weather','temp','atemp','humidity','windspeed','casual','registered']
+cols = ['count','year','month','weekday','hour','season','holiday','workingday','weather','temp','atemp','humidity','windspeed','casual','registered']
 df = df.ix[:,cols]
 df_train = df.iloc[:10886]
 df_test = df.iloc[10886:]
-print (df_train[:10])
 
 # 计算相关系数，查看指标对结果的影响
 correlation = df_train.corr()
@@ -99,18 +98,109 @@ f,ax = plt.subplots(figsize =(16,16))
 cmap = sns.cubehelix_palette(light=1,as_cmap=True)
 sns.heatmap(df.corr(),vmax=1,annot=True,center=1,cmap=cmap,linewidths=1,ax=ax)
 
+from collections import Counter
 # 各指标的影响
-x = [1,2,3,4]
+# 季节的影响
+x = np.unique(df_train['season'])
 y = df_train['count'].groupby(df_train['season']).sum()
-plt.scatter(x,y)
+plt.figure()
+plt.plot(x,y)
+
+# 周几的影响
+x = np.unique(df_train['weekday'])
+y = df_train['count'].groupby(df_train['weekday']).sum()
+plt.figure()
+plt.plot(x,y)
 # print (x)
-plt.show()
 
-sep_list= ['season','weather','weekday','hour']
-df = sep_dummies(df,sep_list)
+# 小时的影响
+x = np.unique(df_train['hour'])
+y = df_train['count'].groupby(df_train['hour']).sum()
+plt.figure()
+plt.plot(x,y)
 
+# 假期的影响
+x = np.unique(df_train['holiday'])
+y = df_train['count'].groupby(df_train['holiday']).sum()
+plt.figure()
+plt.plot(x,y)
+
+# 天气的影响
+x = np.unique(df_train['weather'])
+y = df_train['count'].groupby(df_train['weather']).sum()
+plt.figure()
+plt.plot(x,y)
+
+# 气温的影响
+x = np.unique(df_train['temp'])
+y = df_train['count'].groupby(df_train['temp']).sum()
+plt.figure()
+plt.plot(x,y)
+
+# 气温的影响
+x = np.unique(df_train['windspeed'])
+y = df_train['count'].groupby(df_train['windspeed']).sum()
+plt.figure()
+plt.plot(x,y)
+
+# 湿度的影响
+x = np.unique(df_train['humidity'])
+y = df_train['count'].groupby(df_train['humidity']).sum()
+plt.figure()
+plt.plot(x,y)
+# plt.show()
+
+# 特征工程
+#  删除不要的字段
+df = df.drop(['season','atemp','casual','registered'],axis=1)
+# 离散变量作one-hot处理
+sep_list= ['year','month','weekday','hour','weather']
+df_train = sep_dummies(df_train,sep_list)
+df_test = sep_dummies(df_test,sep_list)
+
+# 构建模型
+# 1、特征向量化
+from sklearn.feature_extraction import DictVectorizer
+col_trans = ['holiday','workingday','temp','humidity','windspeed','year_2011','year_2012','month_1','month_2','month_3','month_4','month_5','month_6','month_7', 'month_8','month_9','month_10', 'month_11','month_12','hour_0','hour_1','hour_2','hour_3','hour_4','hour_5','hour_6','hour_7','hour_8','hour_9','hour_10','hour_11', 'hour_12','hour_13','hour_14','hour_15','hour_16','hour_17','hour_18','hour_19','hour_20','hour_21','hour_22','hour_23','weather_1','weather_2','weather_3','weather_4']
+X_train = df_train[col_trans]
+X_test = df_test[col_trans]
+Y_train = df_train['count']
+vec = DictVectorizer(sparse=False)  # spare = False 是不产生稀疏矩阵
+X_train = vec.fit_transform(X_train.to_dict(orient='record'))
+X_test = vec.fit_transform(X_test.to_dict(orient='record'))
+
+# 分割训练数据
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(X_train,Y_train,test_size=0.25,random_state=40)
+
+# 2、建模预测
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from xgboost import XGBRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import r2_score
+
+# (1)普通随机森林
+rfr = RandomForestRegressor()
+rfr.fit(x_train,y_train)
+rfr_y_predict = rfr.predict(x_test)
+# r2_score(y_test,rfr_y_predict)
+print ('普通随机森林预测准确率为：',r2_score(y_test,rfr_y_predict))
 # print (df_test[:10])
 
+# (2)极端随机森林
+efr = ExtraTreesRegressor()
+efr.fit(x_train,y_train)
+efr_y_predict = efr.predict(x_test)
+# r2_score(y_test,rfr_y_predict)
+print ('极端随机森林预测准确率为：',r2_score(y_test,efr_y_predict))
+
+# （3）人工神经网络
+mlp = MLPRegressor()
+mlp.fit(x_train,y_train)
+mlp_y_predict = mlp.predict(x_test)
+print ('人工神经网络预测准确率为：',r2_score(y_test,mlp_y_predict))
 
 
 # columns_trans= ['season','weather','weekday','hour']
